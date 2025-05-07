8762e11259b037c3464c6dd42caf194dc033b3f5
Nourhane Sarieddine <nourhane.sarieddine@thewebaddicts.com>
2025-05-07 11:28:02 +0300

    update

--- src/Elements/Select.php
+++ src/Elements/Select.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace twa\uikit\Elements;
+
 use Livewire\Attributes\Modelable;
 use Livewire\Component;
 use Illuminate\Support\Facades\DB;
@@ -16,132 +17,151 @@ class Select extends Component
 
 
 
-    public function mount(){
+    public function mount()
+    {
 
         if (!isset($this->info['multiple'])) {
             $this->info['multiple'] = false;
         }
-
     }
-    public function getOptions($search = null , $id = null){
+    public function getOptions($search = null, $id = null)
+    {
 
-        switch($this->info['options']['type']){
-            case "query" :
+        switch ($this->info['options']['type']) {
+
+            case "static":
+
+                // dd("here");
+
+                $options = [];
+
+                if ($search) {
+                    $options = collect($this->info['options']['list'])->filter(function ($item) use ($search) {
+                        return str(strtolower($item['label']))->contains(strtolower($search));
+                    })->values()->toArray();
+                } else {
+                    $options = $this->info['options']['list'];
+                }
+
+                // dd($options);
+
+                break;
+            case "query":
 
                 $parent = null;
-                if(isset($this->info['options']["parent"])){
+                if (isset($this->info['options']["parent"])) {
                     $parent = $this->info['options']["parent"];
                 }
 
                 $options = DB::table($this->info['options']['table']);
 
-                if($parent){
+                if ($parent) {
 
-                   // dd('CONCAT("'.$this->info['name'].'", "_" ,'.$this->info['options']['table'].'.id) as identifier');
+                    // dd('CONCAT("'.$this->info['name'].'", "_" ,'.$this->info['options']['table'].'.id) as identifier');
 
                     // $options->select($this->info['options']['table'].'.id as value' ,
                     // DB::raw('CONCAT('.$parent['table'].'.'.$parent['field'].', " / " , '.$this->info['options']['table'].'.'.$this->info['options']['field'].' ) as label'),
                     // DB::raw('CONCAT("'.$this->info['name'].'", "_" ,'.$this->info['options']['table'].'.id) as identifier'));
 
-                    $options->select($this->info['options']['table'].'.id as value' ,
-                    DB::raw("CONCAT(".$parent['table'].".".$parent['field'].", ' / ' , ".$this->info['options']['table'].".".$this->info['options']['field']." ) as label"),
-                    DB::raw("CONCAT('".$this->info['name']."', '_' ,".$this->info['options']['table'].".id) as identifier"));
+                    $options->select(
+                        $this->info['options']['table'] . '.id as value',
+                        DB::raw("CONCAT(" . $parent['table'] . "." . $parent['field'] . ", ' / ' , " . $this->info['options']['table'] . "." . $this->info['options']['field'] . " ) as label"),
+                        DB::raw("CONCAT('" . $this->info['name'] . "', '_' ," . $this->info['options']['table'] . ".id) as identifier")
+                    );
 
 
-                 $options->leftJoin($parent['table'],
-                 $this->info['options']['table'].'.'.$parent['key'], $parent['table'].'.id');
-
-                }else{
-
-
-                    $options->select($this->info['options']['table'].'.id as value' ,
-                    $this->info['options']['table'].".".$this->info['options']['field'].' as label',
-                    DB::raw("CONCAT('".$this->info['name']."', '_' ,".$this->info['options']['table'].".id) as identifier"));
-                
+                    $options->leftJoin(
+                        $parent['table'],
+                        $this->info['options']['table'] . '.' . $parent['key'],
+                        $parent['table'] . '.id'
+                    );
+                } else {
 
 
-
-                
+                    $options->select(
+                        $this->info['options']['table'] . '.id as value',
+                        $this->info['options']['table'] . "." . $this->info['options']['field'] . ' as label',
+                        DB::raw("CONCAT('" . $this->info['name'] . "', '_' ," . $this->info['options']['table'] . ".id) as identifier")
+                    );
                 }
 
-                $options->whereNull($this->info['options']['table'].'.deleted_at')
-                // ->orWhereIn('id',$this->value)
-                ->limit($this->info['query_limit'] ?? 10)
+                $options->whereNull($this->info['options']['table'] . '.deleted_at')
+                    // ->orWhereIn('id',$this->value)
+                    ->limit($this->info['query_limit'] ?? 10)
 
 
-                ->when($search , function($query) use ($search , $parent){
+                    ->when($search, function ($query) use ($search, $parent) {
 
-                    $words = explode(" " , $search);
+                        $words = explode(" ", $search);
 
-                    foreach($words as $word){
-                        $query->where(function($q) use ($word , $parent) {
-                            $q->orWhere($this->info['options']['table'].'.'.$this->info['options']['field'] , 'LIKE' , $word.'%');
-                            $q->orWhere($this->info['options']['table'].'.'.$this->info['options']['field'] , 'LIKE' , '% '.$word.'%');
-                            if($parent){
-                                $q->orWhere($parent['table'].'.'.$parent['field'] , 'LIKE' , $word.'%');
-                            }
-                        });
-                    }
-                });
-
-
-                foreach($this->info['options']['conditions'] ?? [] as $condition){
-                    apply_condition($options , $condition);
-                }
-               
-
-
-                $options = $options->orderBy('label' , 'ASC');
-
-                if(!is_null($id)){
-                        if(!is_array($id)){
-                            $options->where($this->info['options']['table'].'.'.'id' , $id);
-                        }else{
-                            $options->whereIn($this->info['options']['table'].'.'.'id' , $id);
+                        foreach ($words as $word) {
+                            $query->where(function ($q) use ($word, $parent) {
+                                $q->orWhere($this->info['options']['table'] . '.' . $this->info['options']['field'], 'LIKE', $word . '%');
+                                $q->orWhere($this->info['options']['table'] . '.' . $this->info['options']['field'], 'LIKE', '% ' . $word . '%');
+                                if ($parent) {
+                                    $q->orWhere($parent['table'] . '.' . $parent['field'], 'LIKE', $word . '%');
+                                }
+                            });
                         }
+                    });
+
+
+                foreach ($this->info['options']['conditions'] ?? [] as $condition) {
+                    apply_condition($options, $condition);
+                }
+
+
+
+                $options = $options->orderBy('label', 'ASC');
+
+                if (!is_null($id)) {
+                    if (!is_array($id)) {
+                        $options->where($this->info['options']['table'] . '.' . 'id', $id);
+                    } else {
+                        $options->whereIn($this->info['options']['table'] . '.' . 'id', $id);
+                    }
                 }
 
 
                 $options = $options->get()
-                ->toArray();
+                    ->toArray();
 
 
 
                 break;
-                case "static" :
-                    $options = collect($this->info['options']['list'])->map(function($item){
-                        $item['identifier'] = $this->info['name'].'_'.$item['value'];
-                        return $item;
-                    });
+            case "static":
+                $options = collect($this->info['options']['list'])->map(function ($item) {
+                    $item['identifier'] = $this->info['name'] . '_' . $item['value'];
+                    return $item;
+                });
 
-                    if (!is_null($id)) {
-                        if (!is_array($id)) {
-                        
-                            $options = $options->where('value', $id)->first(); 
-                            $options = $options ? [$options] : [];
-                        } else {
-                          
-                            $options = $options->whereIn('value', $id)->toArray();
-                        }
+                if (!is_null($id)) {
+                    if (!is_array($id)) {
+
+                        $options = $options->where('value', $id)->first();
+                        $options = $options ? [$options] : [];
                     } else {
-                        $options = $options->toArray();
+
+                        $options = $options->whereIn('value', $id)->toArray();
                     }
-                    break;
-                    default :
-                    $options = [];
+                } else {
+                    $options = $options->toArray();
+                }
+                break;
+            default:
+                $options = [];
         }
 
 
         $this->skipRender();
 
         return response()->json($options);
-
     }
 
 
     public function render()
     {
         $options = [];
-        return view('UIKitView::components.form.select' , ['options' =>  $options ]);
+        return view('UIKitView::components.form.select', ['options' =>  $options]);
     }
 }
--- src/Resources/views/components/form/colorpicker.blade.php
+++ src/Resources/views/components/form/colorpicker.blade.php
@@ -4,14 +4,38 @@
     </label>
     <div class="twa-form-input-container">
         <div class="twa-form-input-ring">
-            <input wire:model="value" type="color" class="twa-form-input">
+            <div class="twa-form-input-color" x-data="{
+                color: '#4F46E5',
+                hexOnly: '4F46E5',
+            
+                syncToPicker() {
+                    if (/^[0-9a-fA-F]{6}$/.test(this.hexOnly)) {
+                        this.color = '#' + this.hexOnly;
+                    }
+                },
+            
+                syncToText(e) {
+                    const val = e.target.value.replace('#', '').toUpperCase();
+                    this.hexOnly = val;
+                    this.color = '#' + val;
+                }
+            }" class="flex items-center space-x-1">
+                <span class="text-color-hash">#</span>
+                <input type="text" x-model="hexOnly" maxlength="6" pattern="[0-9a-fA-F]{6}" @input="syncToPicker"
+                    class="text-color w-[100px]" />
+ 
+                <input type="color" :value="color" @input="syncToText($event)" class="picker-color" />
+            </div>
         </div>
+ 
     </div>
-
+ 
     @error(get_field_modal($info) ?? 'value')
         <span class="form-error-message">
             {{ $message }}
         </span>
     @enderror
-
+ 
 </div>
+ 
+ 
