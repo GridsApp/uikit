47ddbb11663994ab93c19de14602b6a96cdec71f
Rozana al halaby <rosana.alhalabi@thewebaddicts.com>
2025-06-04 17:53:17 +0300

    update

--- src/Resources/views/components/toast.blade.php
+++ src/Resources/views/components/toast.blade.php
@@ -1,23 +1,81 @@
-<ul x-data="window.Functions.initToast()" @set-toasts-layout.window="toastLayout()" @toast-show.window="showToast()"
-    @mouseenter="toastsHovered=true;" @mouseleave="toastsHovered=false" x-init="if (layout == 'expanded') { expanded = true; }
+<ul x-data="window.Functions.initToast2()"
+    @set-toasts-layout.window="
+    layout=event.detail.layout;
+    if(layout == 'expanded'){
+        expanded=true;
+    } else {
+        expanded=false;
+    }
     stackToasts();
-   "
+"
+    @toast-show.window="
+    event.stopPropagation();
+    if(event.detail.position){
+        position = event.detail.position;
+    }
+    toasts.unshift({
+        id: 'toast-' + Math.random().toString(16).slice(2),
+        show: false,
+        message: event.detail.message,
+        description: event.detail.description,
+        type: event.detail.type,
+        html: event.detail.html
+    });
+"
+    @mouseenter="toastsHovered=true;" @mouseleave="toastsHovered=false" {{-- x-init="if (layout == 'expanded') { expanded = true; }
+    stackToasts();
+   " --}}
     class="fixed block w-full group z-[99] sm:max-w-xs"
     :class="{ 'right-0 top-0 sm:mt-6 sm:mr-6': position=='top-right', 'left-0 top-0 sm:mt-6 sm:ml-6': position=='top-left', 'left-1/2 -translate-x-1/2 top-0 sm:mt-6': position=='top-center', 'right-0 bottom-0 sm:mr-6 sm:mb-6': position=='bottom-right', 'left-0 bottom-0 sm:ml-6 sm:mb-6': position=='bottom-left', 'left-1/2 -translate-x-1/2 bottom-0 sm:mb-6': position=='bottom-center' }"
     x-cloak>
     <template x-for="(toast, index) in toasts" :key="toast.id">
         <li :id="toast.id" x-data="{
             toastHovered: false
-        }" x-init="init()" @mouseover="toastHovered=true"
+        }" x-init="if (position.includes('bottom')) {
+            $el.firstElementChild.classList.add('toast-bottom');
+            $el.firstElementChild.classList.add('opacity-0', 'translate-y-full');
+        } else {
+            $el.firstElementChild.classList.add('opacity-0', '-translate-y-full');
+        }
+        setTimeout(function() {
+        
+            setTimeout(function() {
+                if (position.includes('bottom')) {
+                    $el.firstElementChild.classList.remove('opacity-0', 'translate-y-full');
+                } else {
+                    $el.firstElementChild.classList.remove('opacity-0', '-translate-y-full');
+                }
+                $el.firstElementChild.classList.add('opacity-100', 'translate-y-0');
+        
+                setTimeout(function() {
+                    stackToasts();
+                }, 10);
+            }, 5);
+        }, 50);
+        
+        setTimeout(function() {
+            setTimeout(function() {
+                $el.firstElementChild.classList.remove('opacity-100');
+                $el.firstElementChild.classList.add('opacity-0');
+                if (toasts.length == 1) {
+                    $el.firstElementChild.classList.remove('translate-y-0');
+                    $el.firstElementChild.classList.add('-translate-y-full');
+                }
+                setTimeout(function() {
+                    deleteToastWithId(toast.id)
+                }, 300);
+            }, 5);
+        }, 4000);" @mouseover="toastHovered=true"
             @mouseout="toastHovered=false" class="absolute w-full duration-300 ease-out select-none sm:max-w-xs"
             :class="{ 'toast-no-description': !toast.description }">
             <span
-                class="relative flex flex-col items-start shadow-[0_5px_15px_-3px_rgb(0_0_0_/_0.08)] w-full transition-all duration-300 ease-out bg-white border border-twafieldsgray-100 sm:rounded-md sm:max-w-xs group"
+                class="relative flex flex-col items-start shadow-[0_5px_15px_-3px_rgb(0_0_0_/_0.08)] w-full transition-all duration-300 ease-out bg-white border border-gray-100 sm:rounded-md sm:max-w-xs group"
                 :class="{ 'p-4': !toast.html, 'p-0': toast.html }">
                 <template x-if="!toast.html">
                     <div class="relative">
                         <div class="flex items-center"
                             :class="{ 'text-green-500': toast.type=='success', 'text-blue-500': toast.type=='info', 'text-orange-400': toast.type=='warning', 'text-red-500': toast.type=='danger', 'text-gray-800': toast.type=='default' }">
+
                             <svg x-show="toast.type=='success'" class="w-[18px] h-[18px] mr-1.5 -ml-1"
                                 viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" clip-rule="evenodd"
@@ -42,7 +100,7 @@
                                     d="M2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12ZM11.9996 7C12.5519 7 12.9996 7.44772 12.9996 8V12C12.9996 12.5523 12.5519 13 11.9996 13C11.4474 13 10.9996 12.5523 10.9996 12V8C10.9996 7.44772 11.4474 7 11.9996 7ZM12.001 14.99C11.4488 14.9892 11.0004 15.4363 10.9997 15.9886L10.9996 15.9986C10.9989 16.5509 11.446 16.9992 11.9982 17C12.5505 17.0008 12.9989 16.5537 12.9996 16.0014L12.9996 15.9914C13.0004 15.4391 12.5533 14.9908 12.001 14.99Z"
                                     fill="currentColor"></path>
                             </svg>
-                            <p class="text-[13px] font-medium leading-none text-twafieldsgray-800" x-text="toast.message"></p>
+                            <p class="text-[13px] font-medium leading-none text-gray-800" x-text="toast.message"></p>
                         </div>
                         <p x-show="toast.description" :class="{ 'pl-5': toast.type!='default' }"
                             class="mt-1.5 text-xs leading-none opacity-70" x-text="toast.description"></p>
@@ -52,14 +110,13 @@
                     <div x-html="toast.html"></div>
                 </template>
                 <span @click="burnToast(toast.id)"
-                    class="absolute right-0 p-1.5 mr-2.5 text-twafieldsgray-400 duration-100 ease-in-out rounded-full opacity-0 cursor-pointer hover:bg-twafieldsgray-50 hover:text-twafieldsgray-500"
+                    class="absolute right-0 p-1.5 mr-2.5 text-gray-400 duration-100 ease-in-out rounded-full opacity-0 cursor-pointer hover:bg-gray-50 hover:text-gray-500"
                     :class="{
                         'top-1/2 -translate-y-1/2': !toast.description && !toast.html,
                         'top-0 mt-2.5': (toast
                             .description || toast.html),
                         'opacity-100': toastHovered,
-                        'opacity-0': !
-                            toastHovered
+                        'opacity-0': !toastHovered
                     }">
                     <svg class="w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
@@ -73,5 +130,5 @@
 </ul>
 
 <div>
-    
+
 </div>
